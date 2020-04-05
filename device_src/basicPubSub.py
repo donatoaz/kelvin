c677aa95295f4b52b25bfe6a148a7d4137253bae
Donato Azevedo <donatoaz@gmail.com>
2020-04-05 18:07:35 -0300

    Simplifies device code, adds some retrying logic

--- device_src/basicPubSub.py
+++ device_src/basicPubSub.py
@@ -1,17 +1,6 @@
 '''
 /*
- * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Kelvin temperature sensor using a DS18b20 1-wire sensor
  */
  '''
 
@@ -24,33 +13,14 @@ import json
 from w1thermsensor import W1ThermSensor, SensorNotReadyError
 from retrying import retry
 
-AllowedActions = ['both', 'publish', 'subscribe']
-
-# Custom MQTT message callback
-def customCallback(client, userdata, message):
-    print("Received a new message: ")
-    print(message.payload)
-    print("from topic: ")
-    print(message.topic)
-    print("--------------\n\n")
-
-
-# Read in command-line parameters
 parser = argparse.ArgumentParser()
 parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
 parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
 parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
 parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
-parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
-parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
-                    help="Use MQTT over WebSocket")
-parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
-                    help="Targeted client id")
+parser.add_argument("-p", "--port", action="store", dest="port", type=int, default=8883, help="Port number override")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub", help="Targeted client id")
 parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
-parser.add_argument("-m", "--mode", action="store", dest="mode", default="both",
-                    help="Operation modes: %s" % str(AllowedActions))
-parser.add_argument("-M", "--message", action="store", dest="message", default="Hello World!",
-                    help="Message to publish")
 
 args = parser.parse_args()
 host = args.host
@@ -58,46 +28,21 @@ rootCAPath = args.rootCAPath
 certificatePath = args.certificatePath
 privateKeyPath = args.privateKeyPath
 port = args.port
-useWebsocket = args.useWebsocket
 clientId = args.clientId
 topic = args.topic
 
-if args.mode not in AllowedActions:
-    parser.error("Unknown --mode option %s. Must be one of %s" % (args.mode, str(AllowedActions)))
-    exit(2)
-
-if args.useWebsocket and args.certificatePath and args.privateKeyPath:
-    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
-    exit(2)
-
-if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
-    parser.error("Missing credentials for authentication.")
-    exit(2)
-
-# Port defaults
-if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
-    port = 443
-if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
-    port = 8883
-
 # Configure logging
 logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 streamHandler.setFormatter(formatter)
 logger.addHandler(streamHandler)
 
 # Init AWSIoTMQTTClient
-myAWSIoTMQTTClient = None
-if useWebsocket:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
-    myAWSIoTMQTTClient.configureEndpoint(host, port)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
-else:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-    myAWSIoTMQTTClient.configureEndpoint(host, port)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, port)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
 # AWSIoTMQTTClient connection configuration
 myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
@@ -107,38 +52,39 @@ myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
 @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
-def connect_to_broker():
+def connect_to_broker_with_retries():
     # Connect and subscribe to AWS IoT
     print("Connecting to broker with retries...")
     try:
         myAWSIoTMQTTClient.connect()
     except connectTimeoutException:
-        raise BaseException("Failed to connect")
-    if args.mode == 'both' or args.mode == 'subscribe':
-        myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
-    time.sleep(2)
+        print("Failed to connect, will retry.")
+        raise
 
 
-connect_to_broker()
+connect_to_broker_with_retries()
+
+# need to sleep before moving on
+time.sleep(2)
+
 # Publish to the same topic in a loop forever
 sensor = W1ThermSensor()
 temperature = 0
 while True:
-    if args.mode == 'both' or args.mode == 'publish':
-        timestamp = int(time.time())
+    timestamp = int(time.time())
 
-        try:
-            temperature = int(10 * sensor.get_temperature())
-            print("Sensor %s has temperature %.2f" % (sensor.id, temperature))
+    try:
+        temperature = int(10 * sensor.get_temperature())
+        print("Sensor %s has temperature %.2f" % (sensor.id, temperature))
 
-            message = {"timestamp": timestamp, "temperature": temperature}
-            messageJson = json.dumps(message)
-            myAWSIoTMQTTClient.publish(topic, messageJson, 1)
-            if args.mode == 'both' or args.mode == 'publish':
-                print('Published topic %s: %s\n' % (topic, messageJson))
-        except SensorNotReadyError:
-            print("Sensor %s is not ready to read")
-        except publishTimeoutException:
-            print("Timedout trying to publish")
+        message = {"timestamp": timestamp, "temperature": temperature}
+        messageJson = json.dumps(message)
+        myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+        print('Published topic %s: %s\n' % (topic, messageJson))
+    except SensorNotReadyError:
+        print("Sensor %s is not ready to read")
+    except publishTimeoutException:
+        print("Timedout trying to publish")
 
-    time.sleep(10)
+    # This will be our polling frequency, once every 30s
+    time.sleep(30)
